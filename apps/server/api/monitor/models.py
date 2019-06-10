1dac155e5b03732117565d22f8c1d5c3dd532067
gmctl <meigea@0528@gmail.com>
2019-06-10 18:04:18 +0800

    -部件用户管理

--- apps/server/api/monitor/models.py
+++ apps/server/api/monitor/models.py
@@ -15,3 +15,12 @@ class ObjProcess(models.Model):
     date_create = models.DateTimeField(verbose_name="发现时间", auto_now_add=True)
 
 
+from ..xobj.models import SysManagerCopInfo, ConnectManagerUserInfo
+
+from django.contrib.auth.models import User
+# 这里把平台用户省略，管理这个models 用 group 
+
+class SystemObjProcess(ObjProcess):
+    cop = models.ForeignKey(SysManagerCopInfo, verbose_name="sys_cop_process", related_name="sys_cop_process", on_delete=models.CASCADE)
+    user = models.ManyToManyField(ConnectManagerUserInfo, verbose_name="系统部件用户", related_name="sys_user_process", on_delete=models.CASCADE)
+    # plat_user = models.ManyToManyField(User, verbose_name="平台用户", related_name="plat_user_process", on_delete=models.CASCADE)
