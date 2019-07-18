022d54f4fbec46d2c7cc1ba56e46256f4363a7fa
gmctl <meigea@0528@gmail.com>
2019-07-18 09:50:52 +0800

    :zap:tini-no-use

--- apps/agent/minitord/snmpd/docker/centos7/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ${SNMP_MODE}='agent' ]; then
-    ${INSTALL_PATH}/sbin/snmpd -C -c /etc/snmpd.conf;
-else
-    ${INSTALL_PATH}/sbin/snmpd -C /etc/snmpd.conf;
-    # /usr/bin/snmptrap -C /software/net-snmp/snmptrap.conf;
-fi
