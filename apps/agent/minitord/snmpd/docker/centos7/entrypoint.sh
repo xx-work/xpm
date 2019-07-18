ca0b0ac5e222d3450afd77622ee1158ff2b0c7bb
gmctl <meigea@0528@gmail.com>
2019-07-18 09:02:24 +0800

    :ambulance:Add-Tini-But_seemd-no-use

--- apps/agent/minitord/snmpd/docker/centos7/entrypoint.sh
+++ apps/agent/minitord/snmpd/docker/centos7/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ${SNMP_MODE}='agent' ]; then
-    ${INSTALL_PATH}/sbin/snmpd -C /etc/snmpd.conf;
+    ${INSTALL_PATH}/sbin/snmpd -C -c /etc/snmpd.conf;
 else
     ${INSTALL_PATH}/sbin/snmpd -C /etc/snmpd.conf;
     # /usr/bin/snmptrap -C /software/net-snmp/snmptrap.conf;
