7c080a68ddfa7fcf846aa1301b3be8663a235b97
gmctl <meigea@0528@gmail.com>
2019-07-18 12:42:26 +0800

    :bug:5.8配置不友好，退到版本5.7.2以及修改snmp用户为root

--- apps/agent/minitord/snmpd/docker/shell.sh
+++ apps/agent/minitord/snmpd/docker/shell.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-yum -y install gcc make perl perl-devel file
-
-
-
+yum -y install gcc make perl perl-devel file ;
 wget https://nchc.dl.sourceforge.net/project/net-snmp/net-snmp/5.8/net-snmp-5.8.tar.gz && \
 tar zxvf net-snmp-5.8.tar.gz && \
 cd net-snmp-5.8 && \
-./configure --prefix=/software/net-snmp \
+./configure --prefix=/snmpd --build=x86_64-linux  \
 --enable-mfd-rewrites \
 --with-default-snmp-version=2 \
 --with-sys-contact=admin@gzqinglang.com \
 --with-sys-location=China \
---with-logfile=/var/log/snmpd.log \
---with-persistent-directory=/var/net-snmp && make && make install
+--with-logfile=/snmpd/var/snmpd.log \
+--with-persistent-directory=/snmpd/var/net-snmp && make && make install && cp EXAMPLE.conf /snmpd/snmpd.conf && rm -rf ../net-snmp-5.8*
+
+/smmpd/sbin/smmpd -f -c -C /snmpd/snmpd.conf
